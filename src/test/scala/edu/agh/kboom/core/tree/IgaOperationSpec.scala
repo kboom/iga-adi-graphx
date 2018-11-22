2150bc69008822ad1d4bbd38ab5e9055d515090c
kbhit <gurgul.grzegorz@gmail.com>
2018-11-22 10:05:27 +0100

    Fixes compilation errors in tests

--- src/test/scala/edu/agh/kboom/core/tree/IgaOperationSpec.scala
+++ src/test/scala/edu/agh/kboom/core/tree/IgaOperationSpec.scala
@@ -2,36 +2,36 @@ package edu.agh.kboom.core.tree
 
 import edu.agh.kboom.core.IgaOperation
 import edu.agh.kboom.core.production._
-import org.scalatest.FunSpec
+import org.scalatest.{FunSpec, Matchers}
 
-class IgaOperationSpec extends FunSpec {
+class IgaOperationSpec extends FunSpec with Matchers {
 
   it("LeafVertex(16) -> BranchVertex(8) = MergeAndEliminateLeaves") {
-    assert(IgaOperation.operationFor(LeafVertex(16), BranchVertex(8)) == IgaOperation(MergeAndEliminateLeaf()))
+    IgaOperation.operationFor(LeafVertex(16), BranchVertex(8)) shouldBe Some(IgaOperation(LeafVertex(16), BranchVertex(8), MergeAndEliminateLeaf))
   }
 
   it("BranchVertex(8) -> InterimVertex(4) = MergeAndEliminateBranch") {
-    assert(IgaOperation.operationFor(BranchVertex(8), InterimVertex(4)) == IgaOperation(MergeAndEliminateInterim()))
+    IgaOperation.operationFor(BranchVertex(8), InterimVertex(4)) shouldBe Some(IgaOperation(BranchVertex(8), InterimVertex(4), MergeAndEliminateInterim))
   }
 
   it("InterimVertex(4) -> InterimVertex(2) = MergeAndEliminateInterim") {
-    assert(IgaOperation.operationFor(InterimVertex(4), InterimVertex(2)) == IgaOperation(MergeAndEliminateInterim()))
+    IgaOperation.operationFor(InterimVertex(4), InterimVertex(2)) shouldBe Some(IgaOperation(InterimVertex(4), InterimVertex(2), MergeAndEliminateInterim))
   }
 
   it("InterimVertex(2) -> RootVertex = MergeAndEliminateRoot") {
-    assert(IgaOperation.operationFor(InterimVertex(2), RootVertex()) == IgaOperation(SolveRoot()))
+    IgaOperation.operationFor(InterimVertex(2), RootVertex()) shouldBe Some(IgaOperation(InterimVertex(2), RootVertex(), SolveRoot))
   }
 
   it("RootVertex -> InterimVertex(2) = BackwardsSubstituteRoot") {
-    assert(IgaOperation.operationFor(RootVertex(), InterimVertex(2)) == IgaOperation(BackwardsSubstituteInterim()))
+    IgaOperation.operationFor(RootVertex(), InterimVertex(2)) shouldBe Some(IgaOperation(RootVertex(), InterimVertex(2), BackwardsSubstituteInterim))
   }
 
   it("InterimVertex(2) -> InterimVertex(4) = BackwardsSubstituteIntermediate") {
-    assert(IgaOperation.operationFor(InterimVertex(2), InterimVertex(4)) == IgaOperation(BackwardsSubstituteBranch()))
+    IgaOperation.operationFor(InterimVertex(2), InterimVertex(4)) shouldBe Some(IgaOperation(InterimVertex(2), InterimVertex(4), BackwardsSubstituteBranch))
   }
 
   it("InterimVertex(4) -> BranchVertex(8) = BackwardsSubstituteBranch") {
-    assert(IgaOperation.operationFor(InterimVertex(4), BranchVertex(8)) == IgaOperation(BackwardsSubstituteLeaf()))
+    IgaOperation.operationFor(InterimVertex(4), BranchVertex(8)) shouldBe Some(IgaOperation(InterimVertex(4), BranchVertex(8), BackwardsSubstituteBranch))
   }
 
 }
