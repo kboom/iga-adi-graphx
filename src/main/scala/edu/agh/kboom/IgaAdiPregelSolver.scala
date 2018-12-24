3b9d9d4c536cac785f59032c4f267e97eba74707
kbhit <gurgul.grzegorz@gmail.com>
2018-12-24 14:34:45 +0100

    Extracts solution from horizontal run

--- src/main/scala/edu/agh/kboom/IgaAdiPregelSolver.scala
+++ src/main/scala/edu/agh/kboom/IgaAdiPregelSolver.scala
@@ -1,9 +1,12 @@
 package edu.agh.kboom
 
 import edu.agh.kboom.core.production.{InitializeLeafMessage, ProductionMessage}
+import edu.agh.kboom.core.tree.ProblemTree.{firstIndexOfBranchingRow, lastIndexOfBranchingRow}
 import edu.agh.kboom.core.tree._
 import edu.agh.kboom.core.{IgaContext, IgaOperation, IgaTasks, Mesh}
 import org.apache.spark.graphx._
+import org.apache.spark.mllib.linalg.{Vector, Vectors}
+import org.apache.spark.mllib.linalg.distributed.{IndexedRow, IndexedRowMatrix, RowMatrix}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 
@@ -38,7 +41,21 @@ object IgaAdiPregelSolver {
         VertexProgram.mergeMsg
       )
 
-    result.vertices.collect().foreach(println)
+    val hs = new IndexedRowMatrix(result.vertices
+      .filterByRange(firstIndexOfBranchingRow(problemTree), lastIndexOfBranchingRow(problemTree))
+      .map { case (v, e) => (v - firstIndexOfBranchingRow(problemTree), e) }
+      .map { case (v, be) => if (v == 0) (v, be.e.mX.arr.dropRight(1)) else (v, be.e.mX.arr.drop(2).dropRight(1)) }
+      .flatMap { case (vid, be) => be.map(Vectors.dense).zipWithIndex.map { case (v, i) => if (vid == 0) IndexedRow(i, v) else IndexedRow(5 + (vid - 1) * 3 + i, v) } }
+    )
+
+    println(f"Size ${hs.numRows()}x${hs.numCols()}")
+
+    hs
+      .rows // Extract RDD[org.apache.spark.mllib.linalg.Vector]
+      .sortBy(_.index)
+      .map(_.vector.toArray.map(i => f"$i%+.3f").mkString(" "))
+      .collect // you can use toLocalIterator to limit memory usage
+      .foreach(println) // Iterate over local Iterator and print
 
 
     // transpose the matrix
@@ -46,6 +63,28 @@ object IgaAdiPregelSolver {
     // https://stackoverflow.com/questions/30556478/matrix-transpose-on-rowmatrix-in-spark
 
     spark.stop()
+  }
+
+  def transposeRowMatrix(m: RowMatrix): RowMatrix = {
+    val transposedRowsRDD = m.rows.zipWithIndex.map { case (row, rowIndex) => rowToTransposedTriplet(row, rowIndex) }
+      .flatMap(x => x) // now we have triplets (newRowIndex, (newColIndex, value))
+      .groupByKey
+      .sortByKey().map(_._2) // sort rows and remove row indexes
+      .map(buildRow) // restore order of elements in each row and remove column indexes
+    new RowMatrix(transposedRowsRDD)
+  }
+
+  def rowToTransposedTriplet(row: Vector, rowIndex: Long): Array[(Long, (Long, Double))] = {
+    val indexedRow = row.toArray.zipWithIndex
+    indexedRow.map { case (value, colIndex) => (colIndex.toLong, (rowIndex, value)) }
+  }
+
+  def buildRow(rowWithIndexes: Iterable[(Long, Double)]): Vector = {
+    val resArr = new Array[Double](rowWithIndexes.size)
+    rowWithIndexes.foreach { case (index, value) =>
+      resArr(index.toInt) = value
+    }
+    Vectors.dense(resArr)
   }
 
 }
