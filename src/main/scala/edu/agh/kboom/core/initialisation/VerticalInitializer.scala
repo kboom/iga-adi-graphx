abf7a1597f5cd82fe1b17db1f6beaac83d13edb8
kbhit <gurgul.grzegorz@gmail.com>
2018-12-27 15:40:35 +0100

    Find (wrong) full solution - to debug

--- src/main/scala/edu/agh/kboom/core/initialisation/VerticalInitializer.scala
+++ src/main/scala/edu/agh/kboom/core/initialisation/VerticalInitializer.scala
@@ -20,7 +20,7 @@ object VerticalInitializer {
     * 10,11,12 - 18
     * 11,12,13 - 19
     */
-  def verticesDependentOnRow(rowNo: Int)(implicit ctx: IgaContext): Seq[Int] = {
+  def verticesDependentOnRow(rowNo: Int)(implicit ctx: IgaContext): Seq[Vertex] = {
     implicit val tree = ctx.yTree()
     val elements = ctx.mesh.yDofs
 
@@ -28,6 +28,7 @@ object VerticalInitializer {
       .map(_ + ProblemTree.firstIndexOfLeafRow)
       .map(_ + rowNo - 1)
       .filterNot { x => x < ProblemTree.firstIndexOfLeafRow || x > ProblemTree.lastIndexOfLeafRow }
+      .map(Vertex.vertexOf)
 
 
     val span = Math.min(3, 1 + Math.min(rowNo, elements - 1 - rowNo))
@@ -35,6 +36,26 @@ object VerticalInitializer {
     return if (rowNo < elements / 2) all.take(span) else all.takeRight(span)
   }
 
+  def findLocalRowFor(v: Vertex, rowNo: Int)(implicit ctx: IgaContext): Int = {
+    implicit val tree = ctx.yTree()
+    return rowNo - Vertex.offsetLeft(v)
+  }
+
+  def findPartitionFor(v: Vertex, rowNo: Int)(implicit ctx: IgaContext): Int = {
+    1
+  }
+
+  def collocate(row: IndexedRow)(implicit ctx: IgaContext): Seq[(Vertex, (Int, Array[Double]))] = {
+    val idx = row.index.toInt
+
+    VerticalInitializer.verticesDependentOnRow(idx)
+      .map(vertex => {
+        val localRow = VerticalInitializer.findLocalRowFor(vertex, idx)
+        val partition = VerticalInitializer.findPartitionFor(vertex, idx)
+        val vertexRowValues = row.vector.toArray.map(_ * partition)
+        (vertex, (localRow, vertexRowValues))
+      })
+  }
 }
 
 case class VerticalInitializer(hsi: Solution) extends LeafInitializer {
@@ -42,49 +63,25 @@ case class VerticalInitializer(hsi: Solution) extends LeafInitializer {
   override def leafData(ctx: IgaContext)(implicit sc: SparkContext): RDD[(VertexId, Element)] = {
     implicit val tree = ctx.yTree()
 
-    val findPartitionFor = (vid: Int, rowNo: Int) => 1
-
-    val findLocalRowFor = (vid: Int, rowNo: Int) => 1
-
-    /*
-id = {0,1,2,3...N}
-
-node.m_b(1)(i) = partition(0) * solution(i)(idx)
-node.m_b(2)(i) = partition(1) * solution(i)(idx + 1)
-node.m_b(3)(i) = partition(2) * solution(i)(idx + 2)
-
-0,1,2
-1,2,3
-2,3,4
-3,4,5
-*/
-    val collocate = (row: IndexedRow) => {
-      val leafCount = ProblemTree.strengthOfLeaves()
-      val idx = row.index.toInt
-
-      VerticalInitializer.verticesDependentOnRow(idx)(ctx)
-        .flatMap(vid => {
-          val localRow = findLocalRowFor(vid, idx)
-          val partition = findPartitionFor(vid, idx)
-          val vertexRowValues = row.vector.toArray.map(_ * partition)
-          Seq((vid.toLong, Seq((localRow, vertexRowValues))))
-        })
-    }
-
     val data = hsi.m.rows
-      .flatMap(collocate)
-      .groupBy(_._1)
-      .flatMapValues(_.map(_._2))
+      .flatMap(m => VerticalInitializer.collocate(m)(ctx))
+      .groupBy(_._1.id.toLong)
+      .mapValues(_.map(_._2))
 
     println(data.collect().map {
-      case (vid, value) => f"$vid: ${value.map(_._1)}"
+      case (vid, value) => f"$vid: ${value}"
     }.mkString(", "))
 
     val leafIndices = firstIndexOfLeafRow to lastIndexOfLeafRow
     sc.parallelize(leafIndices)
       .map(id => (id.toLong, id))
       .join(data)
-      .map { case (idx, d) => (idx.toLong, createElement(Vertex.vertexOf(idx.toInt), d._2.toMap)(ctx)) }
+      .map { case (idx, d) => {
+        val vertex = Vertex.vertexOf(idx.toInt)
+        val value = d._2
+        (idx.toLong, createElement(vertex, value.toMap)(ctx))
+      }
+      }
   }
 
   def createElement(v: Vertex, rows: Map[Int, Array[Double]])(implicit ctx: IgaContext): Element = {
