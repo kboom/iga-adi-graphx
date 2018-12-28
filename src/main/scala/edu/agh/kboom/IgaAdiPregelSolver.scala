faa6e6ff1bd3530454d450db00b40d77a0c7c298
kbhit <gurgul.grzegorz@gmail.com>
2018-12-28 10:59:30 +0100

    Extracts StepSolver

--- src/main/scala/edu/agh/kboom/IgaAdiPregelSolver.scala
+++ src/main/scala/edu/agh/kboom/IgaAdiPregelSolver.scala
@@ -1,10 +1,6 @@
 package edu.agh.kboom
 
 import edu.agh.kboom.core._
-import edu.agh.kboom.core.initialisation.{HorizontalInitializer, VerticalInitializer}
-import edu.agh.kboom.core.tree.ProblemTree
-import org.apache.spark.mllib.linalg.Vectors
-import org.apache.spark.mllib.linalg.distributed.{IndexedRow, IndexedRowMatrix}
 import org.apache.spark.sql.SparkSession
 
 
@@ -17,42 +13,12 @@ object IgaAdiPregelSolver {
 
     // OK: 12, 48
     val mesh = Mesh(24, 24, 24, 24)
-    val solver = DirectionSolver(mesh)
+    val stepSolver = StepSolver(DirectionSolver(mesh))
 
-    implicit val problemTree = ProblemTree(mesh.xSize)
-    implicit val igaContext = IgaContext(mesh, LinearProblem.valueAt)
-
-    val partialSolution = solver.solve(igaContext, HorizontalInitializer)
-    val transposedPartialSolution = Solution(transposeRowMatrix(partialSolution.m))
-    Solution.print(transposedPartialSolution)
-
-    val solution = solver.solve(igaContext.changedDirection(), VerticalInitializer(transposedPartialSolution))
-
-    Solution.print(solution)
-    // transpose the matrix
-
-    // https://stackoverflow.com/questions/30556478/matrix-transpose-on-rowmatrix-in-spark
+    stepSolver.solve(IgaContext(mesh, LinearProblem.valueAt))
 
     spark.stop()
   }
 
-  def transposeRowMatrix(m: IndexedRowMatrix): IndexedRowMatrix = {
-    val transposedRowsRDD = m.rows.map(rowToTransposedTriplet)
-      .flatMap(x => x) // now we have triplets (newRowIndex, (newColIndex, value))
-      .groupByKey
-      .map { case (a, b) => buildRow(a, b) }
-    new IndexedRowMatrix(transposedRowsRDD)
-  }
-
-  def rowToTransposedTriplet(row: IndexedRow): Array[(Long, (Long, Double))] =
-    row.vector.toArray.zipWithIndex.map { case (value, colIndex) => (colIndex.toLong, (row.index, value)) }
-
-  def buildRow(rowIndex: Long, rowWithIndexes: Iterable[(Long, Double)]): IndexedRow = {
-    val resArr = new Array[Double](rowWithIndexes.size)
-    rowWithIndexes.foreach { case (index, value) =>
-      resArr(index.toInt) = value
-    }
-    IndexedRow(rowIndex, Vectors.dense(resArr))
-  }
 
 }
