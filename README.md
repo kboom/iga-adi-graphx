09965a757d376c46095d44bdbab39aa48fd46ecc
kbhit <gurgul.grzegorz@gmail.com>
2019-01-20 11:35:25 +0100

    Adds spark cluster

--- README.md
+++ README.md
@@ -8,3 +8,19 @@ docker pull mesosphere/spark:2.5.0-2.2.1-hadoop-2.7
 
 
 https://stackoverflow.com/questions/37132559/add-jars-to-a-spark-job-spark-submit
+
+
+# RUNNING
+* include a fat jar into spark image
+* run from spark binaries at kubernetes master 
+
+#### Run
+
+Forward SparkUI from kubernetes driver pod
+```bash
+kubectl port-forward $(kubectl get  pods --selector=spark-role=driver --output=jsonpath="{.items..metadata.name}") 4040
+```
+
+#### Performance tuning
+
+https://blog.cloudera.com/blog/2015/03/how-to-tune-your-apache-spark-jobs-part-2/
--- submit.sh
+++ submit.sh
@@ -3,8 +3,13 @@ bin/spark-submit \
     --master k8s://http://localhost:8001 \
     --deploy-mode cluster \
     --name iga-adi-graph \
-    --executor-memory 20G \
-    --total-executor-cores 10 \
-    --class edu.agh.kboom.iga.adi.graph.IgaAdiPregelSolver \
+    --driver-cores 3 \
+    --driver-memory 6G \
+    --executor-cores 3 \
+    --executor-memory 2G \
+    --conf spark.executor.instances=7 \
+    --conf spark.serializer=org.apache.spark.serializer.KryoSerializer \
+    --conf spark.kubernetes.container.image.pullPolicy=Always \
     --conf spark.kubernetes.container.image=kbhit/spark \
-    file:///Users/kbhit/Sources/phd/iga-adi-graphx/target/scala-2.11/iga-adi-graphx_2.11-0.1.0.jar
+    --class edu.agh.kboom.iga.adi.graph.IgaAdiPregelSolver \
+    local:///opt/iga-adi-pregel.jar &
