14bc8d5f9f66ccf2d3f4e3f80b9fd660be83ab6c
kbhit <gurgul.grzegorz@gmail.com>
2019-03-03 22:07:41 +0100

    Refactors iterative solver test

--- src/test/scala/edu/agh/kboom/iga/adi/graph/IterativeSolverIT.scala
+++ src/test/scala/edu/agh/kboom/iga/adi/graph/IterativeSolverIT.scala
@@ -4,7 +4,7 @@ import edu.agh.kboom.MatrixUtils
 import edu.agh.kboom.MatrixUtils.{fill, sumOfIndexes, unit, weakPrecision}
 import edu.agh.kboom.iga.adi.graph.problems.{LinearProblem, OneProblem}
 import edu.agh.kboom.iga.adi.graph.solver._
-import edu.agh.kboom.iga.adi.graph.solver.core.{Mesh, SplineSurface}
+import edu.agh.kboom.iga.adi.graph.solver.core.{Mesh, SplineSurface, StaticProblem}
 
 class IterativeSolverIT extends AbstractIT {
 
@@ -14,30 +14,29 @@ class IterativeSolverIT extends AbstractIT {
     val iterativeSolver = IterativeSolver(solver)
   }
 
+  private def runSolver(solver: IterativeSolver)(p: StaticProblem): SplineSurface = {
+    var lastSurface: Option[SplineSurface] = None
+    solver.solve(p, (surface, si) => {
+      lastSurface = Some(surface)
+      si match {
+        case StepInformation(step, _) => if (step < 2) Some(p) else None
+      }
+    })
+    lastSurface.get
+  }
+
   "running solver for" when {
 
     "element count is 12x12" should {
 
       "should produce valid results for f(x,y) = 1" in new SolverContext(12) {
-        var lastSurface: Option[SplineSurface] = None
-        iterativeSolver.solve(OneProblem, (surface, si) => {
-          lastSurface = Some(surface)
-          si match {
-            case StepInformation(step, _) => if(step < 2) Some(OneProblem) else None
-          }
-        })
-        weakPrecision(SplineSurface.asArray(lastSurface.get)) should equal(MatrixUtils.assembleMatrix(14)(Seq(unit)))
+        val finalSurface = runSolver(iterativeSolver)(OneProblem)
+        weakPrecision(SplineSurface.asArray(finalSurface)) should equal(MatrixUtils.assembleMatrix(14)(Seq(unit)))
       }
 
       "should produce valid results for f(x,y) = x + y" in new SolverContext(12) {
-        var lastSurface: Option[SplineSurface] = None
-        iterativeSolver.solve(LinearProblem, (surface, si) => {
-          lastSurface = Some(surface)
-          si match {
-            case StepInformation(step, _) => if(step < 2) Some(LinearProblem) else None
-          }
-        })
-        weakPrecision(SplineSurface.asArray(lastSurface.get)) should equal(MatrixUtils.assembleMatrix(14)(Seq(fill(-1), sumOfIndexes())))
+        val finalSurface = runSolver(iterativeSolver)(LinearProblem)
+        weakPrecision(SplineSurface.asArray(finalSurface)) should equal(MatrixUtils.assembleMatrix(14)(Seq(fill(-1), sumOfIndexes())))
       }
 
     }
