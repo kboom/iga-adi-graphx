e8ee7968d6a730417fee21a05f90fbc4bb9493e3
kbhit <gurgul.grzegorz@gmail.com>
2019-03-04 21:44:52 +0100

    Outer joining vertices

--- src/main/scala/edu/agh/kboom/iga/adi/graph/solver/DirectionSolver.scala
+++ src/main/scala/edu/agh/kboom/iga/adi/graph/solver/DirectionSolver.scala
@@ -31,9 +31,12 @@ case class DirectionSolver(mesh: Mesh) {
       ).setName("Operation edges")
 
     val graph = Graph.fromEdges(edges, None, MEMORY_ONLY, MEMORY_ONLY)
-      .partitionBy(IgaPartitioner) // todo create an efficient partitioner for IGA-ADI operations
-      .mapVertices((vid, _) => IgaElement(Vertex.vertexOf(vid.toInt)(problemTree), Element.createForX(mesh)))
-      .joinVertices(initializer.leafData(ctx))((_, v, se) => v.swapElement(se))
+      .partitionBy(IgaPartitioner)
+      .mapVertices((vid, _) => Vertex.vertexOf(vid.toInt)(problemTree))
+      .outerJoinVertices(initializer.leafData(ctx))((_, v, se) => se match {
+        case Some(e) => IgaElement(v, e)
+        case None => IgaElement(v, Element.createForX(mesh))
+      })
       .cache() // todo is this really necessary? It greatly reduces the available memory and might not be needed at all
 
     // trigger operations and cache
@@ -74,8 +77,8 @@ case class DirectionSolver(mesh: Mesh) {
           .flatMap { case (vid, be) => (0 until be.rows).view.map(be(_, ::).inner.copy.data)
             .map(Vectors.dense)
             .zipWithIndex
-            .map { case (v, i) => if (vid == 0) IndexedRow(i, v) else IndexedRow(5 + (vid - 1) * 3 + i, v) }.toList
-          }
+            .map { case (v, i) => if (vid == 0) IndexedRow(i, v) else IndexedRow(5 + (vid - 1) * 3 + i, v) }
+          } // do not preserve partitioning here, it totally destroys the performance
       )
   }
 }
