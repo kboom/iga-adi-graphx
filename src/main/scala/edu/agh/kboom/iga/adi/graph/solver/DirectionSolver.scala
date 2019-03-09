1174ed9f0ee126f189bb4019c7943d65b040d5a7
kbhit <gurgul.grzegorz@gmail.com>
2019-03-09 18:00:09 +0100

    Optimises extracting rows

--- src/main/scala/edu/agh/kboom/iga/adi/graph/solver/DirectionSolver.scala
+++ src/main/scala/edu/agh/kboom/iga/adi/graph/solver/DirectionSolver.scala
@@ -66,15 +66,16 @@ case class DirectionSolver(mesh: Mesh) {
     val lastIndex = lastIndexOfBranchingRow(problemTree)
 
     result.vertices
+      .filterByRange(firstIndex, lastIndex)
       .mapPartitions(
-        _.filter { x => x._1 >= firstIndex && x._1 <= lastIndex }
-          .map { case (v, e) => (v - firstIndex, e) }
-          .map { case (v, be) => if (v == 0) (v, be.e.mX(0 to -2, ::)) else (v, be.e.mX(2 to -2, ::)) }
-          .flatMap { case (vid, be) => (0 until be.rows).view.map(be(_, ::).inner.copy.data)
-            .map(Vectors.dense)
-            .zipWithIndex
-            .map { case (v, i) => if (vid == 0) IndexedRow(i, v) else IndexedRow(5 + (vid - 1) * 3 + i, v) }
-          } // do not preserve partitioning here, it totally destroys the performance
+        _.map { case (v, be) =>
+          val local = v - firstIndex
+          if (local == 0) (local, be.e.mX(0 to -2, ::)) else (local, be.e.mX(2 to -2, ::))
+        }.flatMap { case (vid, be) => (0 until be.rows).view.map(be(_, ::).inner.copy.data)
+          .map(Vectors.dense)
+          .zipWithIndex
+          .map { case (v, i) => if (vid == 0) IndexedRow(i, v) else IndexedRow(5 + (vid - 1) * 3 + i, v) }
+        } // do not preserve partitioning here, it totally destroys the performance
       )
   }
 }
