3971dc34b214273e7778111b14862b58a281da65
kbhit <gurgul.grzegorz@gmail.com>
2019-04-13 11:37:32 +0200

    Spark on AWS

--- submit.cluster.sh
+++ submit.cluster.sh
@@ -3,14 +3,14 @@ spark-submit \
     --master yarn \
     --deploy-mode cluster \
     --name iga-adi-graph \
-    --driver-cores 1 \
-    --driver-memory 1G \
-    --executor-cores 1 \
-    --executor-memory 1G \
+    --driver-cores 2 \
+    --driver-memory 2G \
+    --executor-cores 2 \
+    --executor-memory 2G \
     --num-executors 1 \
     --conf spark.executor.extraJavaOptions="" \
-    --conf spark.driver.extraJavaOptions="-Dproblem.size=12 -Dproblem.steps=1" \
+    --conf spark.driver.extraJavaOptions="-Dproblem.size=768 -Dproblem.steps=1" \
     --conf spark.kryo.unsafe=true \
     --class edu.agh.kboom.iga.adi.graph.IgaAdiPregelSolver \
     --jars iga-adi-graphx-assembly-0.1.0.jar \
-    local:///home/ec2-user/iga-adi-graphx-assembly-0.1.0.jar
+    s3n://iga-adi/iga-adi-graphx-assembly-0.1.0.jar
