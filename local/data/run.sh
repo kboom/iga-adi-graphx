7d47bb8fb40b14b70818b222fc7048ebba8a2553
kbhit <gurgul.grzegorz@gmail.com>
2019-05-19 17:03:04 +0200

    Expanded run script

--- local/data/run.sh
+++ local/data/run.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
+PROBLEM_SIZE="${PROBLEM_SIZE:-768}"
 RUN="${RUN:-1}"
 EXEC="${EXEC:-4}"
 EXEC_CORES="${EXEC_CORES:-2}"
 EXEC_MEMORY="${EXEC_MEMORY:-1G}"
 DRIVER_MEMORY="${DRIVER_MEMORY:-1G}"
+JVM_OPTS="${JVM_OPTS:-}"
+DEBUG="${DEBUG:-false}"
+
+if [[ "${DEBUG}" = "true" ]]
+then
+JVM_OPTS="${JVM_OPTS} -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005"
+fi
 
 spark-submit \
     --master spark://$(hostname -i):7077 \
@@ -18,6 +26,6 @@ spark-submit \
     --conf spark.default.parallelism=$(echo "$((${EXEC_CORES} * ${EXEC}))") \
     --conf spark.eventLog.enabled=true \
     --conf spark.eventLog.dir=/tmp/data/logs \
-    --driver-java-options "-Dproblem.size=768 -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005 -XX:+UseCompressedOops -Dproblem.steps=1 -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:+UseStringDeduplication -XX:InitiatingHeapOccupancyPercent=0 -XX:MaxGCPauseMillis=200 -XX:ParallelGCThreads=8 -XX:ConcGCThreads=8 -XX:G1ReservePercent=10 -XX:G1HeapRegionSize=8m" \
+    --driver-java-options "-Dproblem.size=${PROBLEM_SIZE} ${JVM_OPTS} -XX:+UseCompressedOops -Dproblem.steps=1 -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:+UseStringDeduplication -XX:InitiatingHeapOccupancyPercent=0 -XX:MaxGCPauseMillis=200 -XX:ParallelGCThreads=8 -XX:ConcGCThreads=8 -XX:G1ReservePercent=10 -XX:G1HeapRegionSize=8m" \
     ${@} \
     iga-adi-graphx-assembly-0.1.0.jar #&> "${EXEC}-nodes-48-${EXEC_CORES}-${RUN}.txt"
